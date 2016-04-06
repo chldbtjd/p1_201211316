cecafefd4eed3560976324c85cbd6e485615c2f2
chldbtjd <chldbtjd2272@naver.com>
2016-04-06 15:35:13 +0900

    a
    a

--- w6homework/6wMain.py
+++ w6homework/6wMain.py
@@ -9,8 +9,20 @@ def sum(begin,end):
     return sum    
 def lab6():
     sum(begin,end)
+
+
+
+def year():
+    a=raw_input("year:")
+    y=int(a)
+    if(y%4==0) and (y%100 !=0 or y%400==0):
+        print "yoonneyon"
+    else:
+        print "false"   
+def lab6_2():
+    year()
 def main():
     lab6()
+    lab6_2()
 if __name__=="__main__": 
      main() 
-    
