73a0c51dfd34cf2d3f5fbb07ddb18bf8badc8172
chldbtjd <chldbtjd2272@naver.com>
2016-04-07 10:20:41 +0900

    a
    a

--- w6homework/w6Main.py
+++ w6homework/w6Main.py
@@ -18,11 +18,29 @@ def year():
     if(y%4==0) and (y%100 !=0 or y%400==0):
         print "yoonneyon"
     else:
-        print "false"   
+        print "false"
+
+def UpDownGame():
+    while True:
+        a = raw_input("number:")
+        z = int(a)
+        num = 100
+        if(num<z):
+            print "down"
+        elif(num>z):
+            print "up"
+        else:
+            print "correct"
+            break;
+def lab6_3():
+    UpDownGame()
+
+   
 def lab6_2():
     year()
 def main():
     lab6()
     lab6_2()
+    lab6_3()
 if __name__=="__main__": 
      main() 
