c963df3f1fc3c1f1e87ffb74f8e8a67b69979863
chldbtjd <chldbtjd2272@naver.com>
2016-05-03 11:17:44 +0900

    a
    a

--- w8homework/w8Main.py
+++ w8homework/w8Main.py
@@ -1,3 +1,22 @@
+
+import matplotlib
+import matplotlib.pyplot as plt
+    
+def word():
+    word = '7 hongji'
+
+    z=dict()
+    z['alpha']=0
+    z['number']=0
+    for i in word:
+        if i.isalpha():
+                z['alpha']=z['alpha']+1
+        elif i.isdigit():
+                z['number']=z['number']+1
+    print z
+    plt.bar(range(len(z)),z.values(),align='center')
+    plt.xticks(range(len(z)),list(z.keys()))
+    plt.show()
 def dcit():
     word = 'sangmyung university'
     
@@ -11,9 +30,43 @@ def dcit():
             d[i]=d[i]+1
     print d
 
+    d.keys()
+    plt.bar(range(len(d)),d.values(),align='center')
+    plt.xticks(range(len(d)),list(d.keys()))
+    plt.show()
+def Home():
+    Myhome=set(['TV', 'phone', 'fridger', 'mixer', 'audio', 'cd player', 'light', 'computer', 'notebook', 'recorder'])
+    Fhome=set(['coffee machine', 'radio', 'camera', 'running machine', 'ramp', 'computer', 'notebook', 'recorder'])
+    
+    print " MyHome U FriendHome "
+    print Myhome.union(Fhome)
+    
+
+    print "MyHome - FriendHome"
+    print Myhome-Fhome
+    
+    print "FriendHome - MyHome"
+    print Fhome-Myhome
  
+
+    print "FriendHome intersection MyHome"
+    print Myhome.intersection(Fhome)
+    
+
+    num=dict()
+   
+    for c in Myhome.union(Fhome):
+        if c not in Myhome&Fhome:
+            num[c]=1
+        else:
+            num[c]=2
+            
+    print "Count"
+    print num
 def lab8():
         dcit()
+        word()
+        Home()
 def main():
     lab8()
 if __name__=="__main__": 
